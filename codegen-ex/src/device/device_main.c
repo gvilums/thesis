b6487ae94de751d9028556f826de94fa55438122
Georgijs Vilums <georgijs.vilums@gmail.com>
2022-06-12 08:37:02 +0200

    organize output file, change formatting

--- codegen-ex/src/device/device_main.c
+++ codegen-ex/src/device/device_main.c
@@ -1,17 +1,16 @@
 #include <defs.h>
 #include <handshake.h>
 #include <mram.h>
+#include <seqread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <seqread.h>
 
 // assumptions:
 // - LOCAL_BUF_SIZE is always a multiple of INPUT_ELEM_SIZE
 
 #define INPUT_BUF_SIZE (1 << 16)
 #define LOCAL_BUF_SIZE (1 << 8)
-
 
 #define INPUT_TYPE uint32_t
 #define STAGE_0_TYPE uint32_t
@@ -27,7 +26,6 @@
 
 #define INPUT_ELEM_SIZE (sizeof(INPUT_TYPE) * INPUT_ARR_SIZE)
 #define REDUCTION_ELEM_SIZE (sizeof(REDUCTION_TYPE) * REDUCTION_ARR_SIZE)
-
 
 #if INPUT_BUF_SIZE % NR_TASKLETS != 0
 #error "Buffer not evenly divided by number of tasklets"
@@ -57,52 +55,20 @@ __host REDUCTION_TYPE zero_vec[REDUCTION_ARR_SIZE];
 // reduction temporaries (one per thread)
 uint32_t tl_reduction_val[NR_TASKLETS][REDUCTION_ARR_SIZE];
 
-void pipeline_stage_0_map(const INPUT_TYPE* restrict input,
-                          STAGE_0_TYPE* restrict output) {
-    // MAP PROGRAM
-    for (size_t i = 0; i < STAGE_0_ARR_SIZE; ++i) {
-        output[i] = input[i] + input[STAGE_0_ARR_SIZE];
-    }
-    
-}
-
-int pipeline_stage_1_filter(const STAGE_0_TYPE* restrict input) {
-    // FILTER PROGRAM
-    return 1;
-    // return memcmp(input, comparison, STAGE_0_OUT_SIZE) != 0;
-}
-
-void pipeline_stage_2_map(const STAGE_1_TYPE* restrict input,
-                          STAGE_2_TYPE* restrict output) {
-    // MAP PROGRAM
-    for (size_t i = 0; i < STAGE_2_ARR_SIZE; ++i) {
-        output[i] = input[i];
-    }
-    // for (size_t i = 0; i < STAGE_2_OUT_SIZE; i += 2) {
-        // output[i] = input[i];
-        // output[i + 1] = input[i + 1];
-        // if (input[i] < input[i + 1]) {
-        //     output[i] = input[i];
-        //     output[i + 1] = input[i + 1];
-        // } else {
-        //     output[i] = input[i + 1];
-        //     output[i + 1] = input[i];
-        // }
-    // }
-}
-
+void pipeline_stage_0_map(const INPUT_TYPE* restrict input, STAGE_0_TYPE* restrict output);
+int pipeline_stage_1_filter(const STAGE_0_TYPE* restrict input);
+void pipeline_stage_2_map(const STAGE_1_TYPE* restrict input, STAGE_2_TYPE* restrict output);
 void pipeline_reduce(const REDUCTION_TYPE* restrict input0,
                      const REDUCTION_TYPE* restrict input1,
-                     REDUCTION_TYPE* restrict output) {
-    // REDUCE PROGRAM
-    *output = *input0 + *input1;
-}
+                     REDUCTION_TYPE* restrict output);
 
 // PROCESSING PIPELINE
 void pipeline(uint32_t* data_in, uint32_t index) {
-    STAGE_0_TYPE tmp0[STAGE_0_ARR_SIZE];    // stage 0 temporary
-    STAGE_2_TYPE tmp1[STAGE_2_ARR_SIZE];    // stage 2 temporary
+    STAGE_0_TYPE tmp0[STAGE_0_ARR_SIZE];      // stage 0 temporary
+    STAGE_2_TYPE tmp1[STAGE_2_ARR_SIZE];      // stage 2 temporary
     REDUCTION_TYPE tmp2[REDUCTION_ARR_SIZE];  // reduction temporary
+
+    input_type tmp;
 
     // STAGE 0: MAP
     { pipeline_stage_0_map(data_in, tmp0); }
@@ -135,8 +101,7 @@ void reduce(uint32_t index) {
             {
                 // PRE-REDUCE
                 REDUCTION_TYPE tmp[REDUCTION_ARR_SIZE];
-                pipeline_reduce(tl_reduction_val[index],
-                                tl_reduction_val[partner], tmp);
+                pipeline_reduce(tl_reduction_val[index], tl_reduction_val[partner], tmp);
                 memcpy(tl_reduction_val[index], tmp, sizeof(tmp));
             }
         } else {
@@ -155,7 +120,6 @@ int main() {
     size_t input_elem_count = total_input_elems / NR_TASKLETS;
     size_t remaining_elems = total_input_elems % NR_TASKLETS;
 
-
     // distribute N remaining elements onto first N tasklets
     if (index < remaining_elems) {
         input_elem_count += 1;
@@ -169,11 +133,11 @@ int main() {
     // init local reduction variable
     memcpy(tl_reduction_val[index], zero_vec, sizeof(zero_vec));
 
-
     seqreader_buffer_t local_cache = seqread_alloc();
     seqreader_t sr;
 
-    INPUT_TYPE* current_read = seqread_init(local_cache, &input_data_buffer[data_offset * INPUT_ELEM_SIZE], &sr);
+    INPUT_TYPE* current_read =
+        seqread_init(local_cache, &input_data_buffer[data_offset * INPUT_ELEM_SIZE], &sr);
 
     for (size_t i = 0; i < input_elem_count; ++i) {
         pipeline(current_read, index);
@@ -187,4 +151,54 @@ int main() {
         printf("successfully executed pipeline\n");
     }
     return 0;
+}
+
+#undef IN_SIZE
+#undef OUT_SIZE
+#define IN_SIZE INPUT_ARR_SIZE
+#define OUT_SIZE STAGE_0_ARR_SIZE
+
+void pipeline_stage_0_map(const INPUT_TYPE* restrict input, STAGE_0_TYPE* restrict output) {
+    // MAP PROGRAM
+    for (size_t i = 0; i < STAGE_0_ARR_SIZE; ++i) {
+        output[i] = input[i] + input[STAGE_0_ARR_SIZE];
+    }
+}
+
+
+#undef IN_SIZE
+#undef OUT_SIZE
+#define IN_SIZE INPUT_ARR_SIZE
+#define OUT_SIZE STAGE_0_ARR_SIZE
+
+int pipeline_stage_1_filter(const STAGE_0_TYPE* restrict input) {
+    // FILTER PROGRAM
+    return 1;
+    // return memcmp(input, comparison, STAGE_0_OUT_SIZE) != 0;
+}
+
+#undef IN_SIZE
+#undef OUT_SIZE
+#define IN_SIZE INPUT_ARR_SIZE
+#define OUT_SIZE STAGE_0_ARR_SIZE
+
+void pipeline_stage_2_map(const STAGE_1_TYPE* restrict input, STAGE_2_TYPE* restrict output) {
+    // MAP PROGRAM
+    for (size_t i = 0; i < STAGE_2_ARR_SIZE; ++i) {
+        output[i] = input[i];
+    }
+}
+
+#undef IN_SIZE
+#undef OUT_SIZE
+#undef ACCUM_SIZE
+#define IN_SIZE INPUT_ARR_SIZE
+#define OUT_SIZE STAGE_0_ARR_SIZE
+#define ACCUM_SIZE STAGE_0_ARR_SIZE
+
+void pipeline_reduce(const REDUCTION_TYPE* restrict input0,
+                     const REDUCTION_TYPE* restrict input1,
+                     REDUCTION_TYPE* restrict output) {
+    // REDUCE PROGRAM
+    *output = *input0 + *input1;
 }

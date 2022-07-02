aa9db0ce20094a5d2ab25e050a3938203d464f02
Georgijs Vilums <georgijs.vilums@gmail.com>
2022-07-02 16:52:51 +0200

    implement and integrate parameter optimization pipeline

--- codegen-ex/pipeline.py
+++ codegen-ex/pipeline.py
@@ -1,11 +1,48 @@
 from mako.template import Template
 from mako.lookup import TemplateLookup
+from enum import Enum
 import tomllib
 import sys
 import dataclasses
 import subprocess
 import re
 import tempfile
+import math
+
+def main():
+    if len(sys.argv) != 3:
+        print(f"usage: {sys.argv[0]} <configuration file> <output directory>")
+        return
+    config_name = sys.argv[1]
+    output_dir = sys.argv[2]
+    lookup = TemplateLookup(directories=["templates/base", "templates/reduce", "templates/noreduce", "templates/util"])
+
+    config = read_config(config_name)
+
+    normalize_config(config)
+    config["pipeline"]["nr_tasklets"] = 11
+
+    if config["stages"][-1]["kind"] == "reduce":
+        generator = create_reduce_pipeline
+    else:
+        generator = create_noreduce_pipeline
+
+    base_code = generator(config, lookup)
+
+    size_info = compute_size_info(config, lookup, base_code)
+    if config["stages"][-1]["kind"] == "reduce":
+        params = optimize_reduce_params(size_info)
+    else:
+        params = optimize_noreduce_params(size_info)
+
+    print(params)
+    print(size_info)
+    
+    params.apply_to(config)
+
+    final_code = generator(config, lookup)
+    final_code.output_to(output_dir)
+
 
 @dataclasses.dataclass
 class CodegenOutput:
@@ -27,15 +64,44 @@ class CodegenOutput:
         with open(f'{output_dir}/host.h', 'w') as out:
             out.write(self.host_header)
 
+
 @dataclasses.dataclass
 class SizeInformation:
     stack_size: int
+    global_sizes: list[int]
+    constant_sizes: list[int]
     input_sizes: list[int]
     output_size: int
+
+
+@dataclasses.dataclass
+class ReduceConfigParams:
+    nr_tasklets: int
+    nr_reduction_vars: int
+    read_buf_size: int
+
+    def apply_to(self, config):
+        config["pipeline"]["nr_tasklets"] = self.nr_tasklets
+        config["pipeline"]["reduction_vars"] = self.nr_reduction_vars
+        config["pipeline"]["read_buf_size"] = self.read_buf_size
+
+
+@dataclasses.dataclass
+class NoreduceConfigParams:
+    nr_tasklets: int
+    read_buf_size: int
+    write_buf_size: int
+
+    def apply_to(self, config):
+        config["pipeline"]["nr_tasklets"] = self.nr_tasklets
+        config["pipeline"]["read_buf_size"] = self.read_buf_size
+        config["pipeline"]["write_buf_size"] = self.write_buf_size
+
 
 def read_config(filename: str):
     with open(filename, "rb") as f:
         return tomllib.load(f)
+
 
 def compute_input_indices(stages):
     stages[0]["input_idx"] = -1
@@ -45,9 +111,11 @@ def compute_input_indices(stages):
             last_stable = prev_idx
         stage["input_idx"] = last_stable
 
+
 def index_stages(stages):
     for idx, stage in enumerate(stages):
         stage["id"] = idx
+
 
 def compute_filter_info(config):
     contains_filter = False
@@ -63,22 +131,18 @@ def compute_filter_info(config):
                 stage["last_filter"] = True
                 break
 
-def create_reduce_pipeline(data, lookup: TemplateLookup) -> CodegenOutput:
+
+def create_reduce_pipeline(config, lookup: TemplateLookup) -> CodegenOutput:
 
     device_code_template = lookup.get_template("device_reduce.c")
     common_header_template = lookup.get_template("common_reduce.h")
     host_code_template = lookup.get_template("host_reduce.c")
     host_header_template = lookup.get_template("host_reduce.h")
 
-    compute_input_indices(data["stages"])
-    index_stages(data["stages"])
-
-    pipeline = data["pipeline"]
-    in_stage = data["stages"][0]
-    stages = data["stages"][1:len(data["stages"]) - 1]
-    reduction = data["stages"][-1]
-
-    pipeline["reduction_vars"] = 16
+    pipeline = config["pipeline"]
+    in_stage = config["stages"][0]
+    stages = config["stages"][1:len(config["stages"]) - 1]
+    reduction = config["stages"][-1]
 
     device_code = device_code_template.render(pipeline=pipeline, stages=stages, reduction=reduction, in_stage=in_stage)
     common_header = common_header_template.render(pipeline=pipeline, stages=stages, reduction=reduction, in_stage=in_stage)
@@ -87,21 +151,17 @@ def create_reduce_pipeline(data, lookup: TemplateLookup) -> CodegenOutput:
 
     return CodegenOutput(device_code, common_header, host_code, host_header)
 
-def create_noreduce_pipeline(data, lookup: TemplateLookup) -> CodegenOutput:
+
+def create_noreduce_pipeline(config, lookup: TemplateLookup) -> CodegenOutput:
     device_code_template = lookup.get_template("device_noreduce.c")
     common_header_template = lookup.get_template("common_noreduce.h")
     host_code_template = lookup.get_template("host_noreduce.c")
     host_header_template = lookup.get_template("host_noreduce.h")
 
-    data["stages"].append({"kind": "output"})
-    compute_input_indices(data["stages"])
-    index_stages(data["stages"])
-    compute_filter_info(data)
-
-    pipeline = data["pipeline"]
-    in_stage = data["stages"][0]
-    stages = data["stages"][1:len(data["stages"]) - 1]
-    output = data["stages"][-1]
+    pipeline = config["pipeline"]
+    in_stage = config["stages"][0]
+    stages = config["stages"][1:len(config["stages"]) - 1]
+    output = config["stages"][-1]
 
     device_code = device_code_template.render(pipeline=pipeline, stages=stages, output=output, in_stage=in_stage)
     common_header = common_header_template.render(pipeline=pipeline, stages=stages, output=output, in_stage=in_stage)
@@ -110,9 +170,12 @@ def create_noreduce_pipeline(data, lookup: TemplateLookup) -> CodegenOutput:
 
     return CodegenOutput(device_code, common_header, host_code, host_header)
 
+
 def normalize_config(config):
     assert "pipeline" in config
     assert len(config["stages"]) > 0
+
+    has_reduce = False
 
     assert "stages" in config
     for i, stage in enumerate(config["stages"]):
@@ -130,7 +193,11 @@ def normalize_config(config):
         else:
             assert stage["kind"] in ["map", "filter", "reduce"]
         if stage["kind"] == "reduce":
+            has_reduce = True
             assert i == len(config["stages"]) - 1
+    
+    if not has_reduce:
+        config["stages"].append({"kind": "output"})
 
     if "globals" not in config["pipeline"]:
         config["pipeline"]["globals"] = []
@@ -138,52 +205,92 @@ def normalize_config(config):
     if "constants" not in config["pipeline"]:
         config["pipeline"]["constants"] = []
     
-
-
-def main():
-    if len(sys.argv) != 3:
-        print(f"usage: {sys.argv[0]} <configuration file> <output directory>")
-        return
-    config_name = sys.argv[1]
-    output_dir = sys.argv[2]
-    lookup = TemplateLookup(directories=["templates/base", "templates/reduce", "templates/noreduce", "templates/util"])
-
-    config = read_config(config_name)
-
-    normalize_config(config)
-    config["pipeline"]["nr_tasklets"] = 16
-
-    if config["stages"][-1]["kind"] == "reduce":
-        base_code = create_reduce_pipeline(config, lookup)
-    else:
-        base_code = create_noreduce_pipeline(config, lookup)
-
-    size_info = compute_size_info(config, lookup, base_code)
-    print(size_info)
+    # default parameters for initial codegen
+    config["pipeline"]["nr_tasklets"] = 11
+    config["pipeline"]["reduction_vars"] = 11
+    config["pipeline"]["read_buf_size"] = 512
+    config["pipeline"]["write_buf_size"] = 512
     
-    base_code.output_to(output_dir)
-
+    # various indexing operations required for codegen
+    compute_input_indices(config["stages"])
+    index_stages(config["stages"])
+    compute_filter_info(config)
+    
 
 def compute_size_info(config, lookup: TemplateLookup, code: CodegenOutput) -> SizeInformation:
     size_info_template = lookup.get_template("size_info.c")
     num_inputs = len(config["stages"][0]["inputs"])
+    num_globals = len(config["pipeline"]["globals"])
+    num_constants = len(config["pipeline"]["constants"])
     with tempfile.TemporaryDirectory() as tmpdir:
         code.output_to(tmpdir)
         with open(f"{tmpdir}/size_info.c", "w") as f:
-            f.write(size_info_template.render(num_inputs=num_inputs))
+            f.write(size_info_template.render(num_inputs=num_inputs, num_globals=num_globals, num_constants=num_constants))
 
         subprocess.run(["dpu-upmem-dpurte-clang", "-DSTACK_SIZE_DEFAULT=1024", "-DNR_TASKLETS=16", "-g", "-O2", f"{tmpdir}/device.c", "-o", f"{tmpdir}/device"], check=True)
         stack_analysis_output = subprocess.run(["dpu_stack_analyzer", f"{tmpdir}/device"], capture_output=True).stdout.decode("utf8")
         stack_size_re_res = re.search(r"Max size: (\d+)\n", stack_analysis_output)
-        stack_size = int(stack_size_re_res.group(1))
+        # align stack size to 8 bytes
+        stack_size = ((int(stack_size_re_res.group(1)) - 1) | 7) + 1
 
         subprocess.run(["gcc", f"{tmpdir}/size_info.c", "-o", f"{tmpdir}/size_info"], check=True)
         size_info_output = subprocess.run([f"{tmpdir}/size_info"], capture_output=True).stdout.decode("utf8").split()
-        assert len(size_info_output) == num_inputs + 1
-        input_sizes = list(map(int, size_info_output[:-1]))
+        assert len(size_info_output) == num_globals + num_constants + num_inputs + 1
+        global_sizes = list(map(int, size_info_output[0:num_globals]))
+        constant_sizes = list(map(int, size_info_output[num_globals:(num_globals + num_constants)]))
+        input_sizes = list(map(int, size_info_output[(num_globals + num_constants):-1]))
         output_size = int(size_info_output[-1])
 
-        return SizeInformation(stack_size, input_sizes, output_size)
+        return SizeInformation(stack_size, global_sizes, constant_sizes, input_sizes, output_size)
+
+
+def optimize_reduce_params(sizes: SizeInformation) -> ReduceConfigParams:
+    if max(sizes.input_sizes) > 2048:
+        raise "input element too large"
+
+    num_inputs = len(sizes.input_sizes)
+    
+    base_size = pow(2, 16) # default WRAM size is 64KB == 2^16 B
+    base_size -= sum(sizes.global_sizes) + sum(sizes.constant_sizes)
+
+    input_buf_size = (base_size / 11 - sizes.stack_size - sizes.output_size) / num_inputs
+    input_buf_size = min(pow(2, math.floor(math.log2(input_buf_size))), 2048)
+
+    # check if valid 
+    if input_buf_size >= 32 and all(map(lambda x: x <= input_buf_size, sizes.input_sizes)):
+        return ReduceConfigParams(11, 11, input_buf_size)
+
+    # decide between (T == 11 and R < T) or (T == R and T < 11)
+    # for now, assume that we want to stay at T == 11. Reduce R instead
+    
+    # reasonable choice for input buf size: enough to fit any input, but not smaller than 256
+    input_buf_size = max(pow(2, math.ceil(math.log2(max(sizes.input_sizes)))), 256)
+    reduction_var_count = (base_size - 11 * (sizes.stack_size + input_buf_size * num_inputs)) // sizes.output_size
+
+    if reduction_var_count >= 1:
+        return ReduceConfigParams(11, reduction_var_count, input_buf_size)
+
+    raise "unable to configure program parameters"
+
+
+def optimize_noreduce_params(sizes: SizeInformation) -> NoreduceConfigParams:
+    if max(sizes.input_sizes) > 2048:
+        raise "input element too large"
+
+    num_inputs = len(sizes.input_sizes)
+
+    base_size = pow(2, 16) # default WRAM size is 64KB == 2^16 B
+    base_size -= sum(sizes.global_sizes) + sum(sizes.constant_sizes)
+
+    buf_size = (base_size / 11 - sizes.stack_size) / (num_inputs + 1)
+    buf_size = min(pow(2, math.floor(math.log2(buf_size))), 2048)
+
+    if buf_size >= 32 and all(map(lambda x: x <= buf_size, sizes.input_sizes + [sizes.output_size])):
+        return NoreduceConfigParams(11, buf_size, buf_size)
+
+    # TODO: in case the buffers don't fit, try reducing number of tasklets
+    
+    raise "unable to configure program parameters"
 
 
 if __name__ == "__main__":
--- codegen-ex/templates/util/size_info.c
+++ codegen-ex/templates/util/size_info.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 
 int main() {
+% for i in range(num_globals):
+    printf("%lu\n", sizeof(global_${ i }_t));
+% endfor
+% for i in range(num_constants):
+    printf("%lu\n", sizeof(constant_${ i }_t));
+% endfor
 % for i in range(num_inputs):
     printf("%lu\n", sizeof(input_${ i }_t));
 % endfor

b9a026c24c687c4df591a93c78aee5cce2a4530d
Georgijs Vilums <georgijs.vilums@gmail.com>
2022-07-14 09:19:14 +0200

    implement plotting code

--- codegen-ex/eval.py
+++ codegen-ex/eval.py
@@ -1,18 +1,40 @@
+#!/usr/bin/python3.10
+
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import os
 import json
 
-def create_plot():
-    data1 = [23,85, 72, 43, 52]
-    data2 = [42, 35, 21, 16, 9]
-    width = 0.2
+def create_plot(name: str, data: dict[str, list[float]]):
+    data1 = data["base"]
+    data2 = data["someopt"]
+    data3 = data["fullopt"]
 
-    fig, plots = plt.subplots(2, 5)
-    plt.bar(np.arange(len(data1)), data1, width=width)
-    plt.bar(np.arange(len(data2)) + width, data2, width=width)
-    plt.savefig('data.png')
+    print(name)
+    print(data1)
+    print(data2)
+    print(data3)
+
+    wd = 0.3
+
+    x_pos = np.arange(1, 2*len(data1), 2)
+
+    plt.figure()
+
+    # plt.semilogy()
+    plt.bar(x_pos, data1, color='r', width=wd, label="O0", edgecolor='k')
+    plt.bar(x_pos + wd, data2, color='y', width=wd, label="O1", edgecolor='k')
+    plt.bar(x_pos + 2 * wd, data3, color='c', width=wd, label="O2", edgecolor='k')
+
+    plt.xticks(x_pos+wd, ["cpu -> dpu", "dpu compute", "dpu -> cpu", "cpu merge", "total"], fontsize=10)
+    plt.yticks(fontsize=10)
+    plt.title(f"{name}", fontsize=15)
+    plt.ylabel('us', fontsize=10)
+
+    plt.legend(loc="upper center", fontsize=10)
+
+    plt.savefig(f"results/plot_{name}.png", dpi=300)
 
 def read_inputs(dir, base, someopt, fullopt, reference, cpu):
     inputs = {}
@@ -31,8 +53,8 @@ def read_inputs(dir, base, someopt, fullopt, reference, cpu):
                 inputs[name][category] = data
 
     read_file(base, "base")
-    # read_file(someopt, "someopt")
-    # read_file(fullopt, "fullopt")
+    read_file(someopt, "someopt")
+    read_file(fullopt, "fullopt")
     # read_file(reference, "reference")
     # read_file(cpu, "cpu")
 
@@ -40,8 +62,8 @@ def read_inputs(dir, base, someopt, fullopt, reference, cpu):
 
 
 def main():
-    out = read_inputs("results", "example.txt", None, None, None, None)
+    out = read_inputs("results", "out_O0.csv", "out_O1.csv", "out_O2.csv", None, None)
     for k, val in out.items():
-        print(k, val)
+        create_plot(k, val)
 
 main()

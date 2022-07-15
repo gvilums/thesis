9ba5d48b1d0eae4f53d3f87f908e12d4ec081489
Georgijs Vilums <georgijs.vilums@gmail.com>
2022-07-15 16:29:34 +0200

    udpate histogram_large code to be more robust

--- codegen-ex/ref/run_all.bash
+++ codegen-ex/ref/run_all.bash
@@ -1,9 +1,28 @@
 #!/usr/bin/bash
 rm -f ref_out.csv
 echo "name, cpu -> dpu, dpu compute, dpu -> cpu, combine, total" >> ./ref_out.csv
-for dir in ./tests/*; do
-    cd $dir
-    make
-    ./bin/host_code >> ../../ref_out.csv
-    cd ../..
-done
+
+cd tests/histogram_large
+make
+./bin/host_code >> ../../ref_out.csv
+cd ../..
+
+cd tests/histogram_small
+make
+./bin/host_code >> ../../ref_out.csv
+cd ../..
+
+cd tests/select
+make
+./bin/host_code >> ../../ref_out.csv
+cd ../..
+k
+cd tests/sum_reduce
+make
+./bin/host_code >> ../../ref_out.csv
+cd ../..
+
+cd tests/vector_add
+make
+./bin/host_code >> ../../ref_out.csv
+cd ../..
